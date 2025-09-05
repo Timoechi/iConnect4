bd5ecec44f4d6f00421688119960e51abe76f6bc
Timoechi <timothychow.priv@gmail.com>
2025-09-04 21:26:39 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -71,54 +71,190 @@ int main(int argc, char **argv)
   P.play("4");
   gameHistory = "4";
   // test input
-  P.play("1");
-  gameHistory += "1";
+  P.play("3");
+  gameHistory += "3";
   std::cout << gameHistory << "\n";
 
   // opening
   switch (std::stoi(gameHistory))
   {
   case 41:
-    P.play("4");
-    gameHistory += "4";
+    P.play("4"); gameHistory += "4";
     break;
   case 42:
-    P.play("2");
-    gameHistory += "2";
+    P.play("2"); gameHistory += "2";
     break;
   case 43:
-    P.play("6");
-    gameHistory += "6";
+    P.play("6"); gameHistory += "6";
     break;
   case 44:
-    P.play("4");
-    gameHistory += "4";
+    P.play("4"); gameHistory += "4";
     break;
   case 45:
-    P.play("2");
-    gameHistory += "2";
+    P.play("2"); gameHistory += "2";
     break;
   case 46:
-    P.play("6");
-    gameHistory += "6";
+    P.play("6"); gameHistory += "6";
     break;
   case 47:
-    P.play("7");
-    gameHistory += "7";
+    P.play("7"); gameHistory += "7";
     break;
   }
   std::cout << gameHistory << "\n";
 
   // test input
-  P.play("1");
-  gameHistory += "1";
+  P.play("4");
+  gameHistory += "4";
   std::cout << gameHistory << "\n";
 
   switch (std::stoi(gameHistory))
   {
   case 4141:
-    P.play("5");
-    gameHistory += "5";
+    P.play("5"); gameHistory += "5";
+    break;
+  case 4142:
+    P.play("6"); gameHistory += "6";
+    break;
+  case 4143:
+    P.play("4"); gameHistory += "4";
+    break;
+  case 4144:
+    P.play("4"); gameHistory += "4";
+    break;
+  case 4145:
+    P.play("4"); gameHistory += "4";
+    break;
+  case 4146:
+    P.play("4"); gameHistory += "4";
+    break;
+  case 4147:
+    P.play("4"); gameHistory += "4";
+    break;
+  case 4221:
+    P.play("5"); gameHistory += "5";
+    break;
+  case 4222:
+    P.play("4"); gameHistory += "4";
+    break;
+  case 4223:
+    P.play("6"); gameHistory += "6";
+    break;
+  case 4224:
+    P.play("4"); gameHistory += "4";
+    break;
+  case 4225:
+    P.play("4"); gameHistory += "4";
+    break;
+  case 4226:
+    P.play("4"); gameHistory += "4";
+    break;
+  case 4227:
+    P.play("4"); gameHistory += "4";
+    break;
+  case 4361:
+    P.play("4"); gameHistory += "4";
+    break;
+  case 4362:
+    P.play("4"); gameHistory += "4";
+    break;
+  case 4363:
+    P.play("6"); gameHistory += "6";
+    break;
+  case 4364:
+    P.play("4"); gameHistory += "4";
+    break;
+  case 4365:
+    P.play("5"); gameHistory += "5";
+    break;
+  case 4366:
+    P.play("7"); gameHistory += "7";
+    break;
+  case 4367:
+    P.play("6"); gameHistory += "6";
+    break;
+  case 4441:
+    P.play("5"); gameHistory += "5";
+    break;
+  case 4442:
+    P.play("4"); gameHistory += "4";
+    break;
+  case 4443:
+    P.play("3"); gameHistory += "3";
+    break;
+  case 4444:
+    P.play("4"); gameHistory += "4";
+    break;
+  case 4445:
+    P.play("4"); gameHistory += "4";
+    break;
+  case 4446:
+    P.play("4"); gameHistory += "4";
+    break;
+  case 4447:
+    P.play("3"); gameHistory += "3";
+    break;
+  case 4521:
+    P.play("2"); gameHistory += "2";
+    break;
+  case 4522:
+    P.play("1"); gameHistory += "1";
+    break;
+  case 4523:
+    P.play("3"); gameHistory += "3";
+    break;
+  case 4524:
+    P.play("4"); gameHistory += "4";
+    break;
+  case 4525:
+    P.play("2"); gameHistory += "2";
+    break;
+  case 4526:
+    P.play("4"); gameHistory += "4";
+    break;
+  case 4527:
+    P.play("4"); gameHistory += "4";
+    break;
+  case 4661:
+    P.play("4"); gameHistory += "4";
+    break;
+  case 4662:
+    P.play("4"); gameHistory += "4";
+    break;
+  case 4663:
+    P.play("4"); gameHistory += "4";
+    break;
+  case 4664:
+    P.play("4"); gameHistory += "4";
+    break;
+  case 4665:
+    P.play("6"); gameHistory += "6";
+    break;
+  case 4666:
+    P.play("4"); gameHistory += "4";
+    break;
+  case 4667:
+    P.play("6"); gameHistory += "6";
+    break;
+  case 4741:
+    P.play("4"); gameHistory += "4";
+    break;
+  case 4742:
+    P.play("4"); gameHistory += "4";
+    break;
+  case 4743:
+    P.play("4"); gameHistory += "4";
+    break;
+  case 4744:
+    P.play("4"); gameHistory += "4";
+    break;
+  case 4745:
+    P.play("4"); gameHistory += "4";
+    break;
+  case 4746:
+    P.play("4"); gameHistory += "4";
+    break;
+  case 4747:
+    P.play("4"); gameHistory += "4";
     break;
   }
   std::cout << gameHistory << "\n";
